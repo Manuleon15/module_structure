4522857ab9029626b9e09f0c2cc69794ed131ea2
Manuleon15 <113500909+Manuleon15@users.noreply.github.com>
2023-10-26 19:50:38 +0200

    1st_assigment

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # module_structure
+
+Este repositorio está destinado a almacenar todas las prácticas y/o código utilizado
+en clase para la asignatura de Machine Learning
+
+## Contenido 
+
+- [Requisitos](#requisitos)
+
+## Requisitos
+Es necesario una versión de python desde 3.9.0 en adelante y se utilizará la librería pytorch
---
+++ module_structure/1st_assigment.py
@@ -0,0 +1,42 @@
+from torch import Tensor
+
+
+class Operations:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def zeros(shape):
+        return torch.zeros(shape)
+
+    @staticmethod
+    def ones(shape):
+        return torch.zeros(shape)
+
+    @staticmethod
+    def random(shape):
+        return torch.rand(shape)
+
+    @staticmethod
+    def sum(t1, t2):
+        return t1 + t2
+
+    @staticmethod
+    def mult(t1, t2):
+        return t1 * t2
+
+
+op = Operations
+
+op.zeros(2)
+
+op.ones(2)
+
+op.random(3)
+
+tensor1 = torch.tensor([[3, 2, 5], [2, 4, 5]])
+tensor2 = torch.tensor([[1, 5, 7], [6, 3, 1]])
+
+op.sum(tensor1, tensor2)
+
+op.mult(tensor1, tensor2)
