e917f96fabc5119a828b59e04a2f40de292845fa
Manuel León <manulionpo@gmail.com>
2023-11-07 17:16:22 +0100

    1st_assigment

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup, find_packages
-from module_structure import __name__
-
+from assigments import __name__
 
 NAME = __name__
 
 setup(
-    name                    = NAME,
-    description             = 'Brief description of your package',
-    author_email            = 'manulionpo@gmail.com',
-    license                 = 'MIT',
-    python_requires         = '>=3.9.5',
-    packages                = find_packages(),
-    include_package_data    = True,
-    package_data            = {'': ['resources/.csv','resources/clusters/.csv']},
-    install_requires        = [
-                                'pandas',
-                                'numpy',
-                                'torch'
-                                ]
-     )
+    name=NAME,
+    description='Brief description of your package',
+    author_email='manulionpo@gmail.com',
+    license='MIT',
+    python_requires='>=3.9.5',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['resources/.csv', 'resources/clusters/.csv']},
+    install_requires=[
+        'pandas',
+        'numpy',
+        'torch'
+    ]
+)
